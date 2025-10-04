1a7e57fcb278aae939c17b8ce2d4d464364d452f
ajalmaguer <ajalmaguer@users.noreply.github.com>
2025-10-04 22:31:53 +0000

    deploy: 956e657680b90c8afff3482b9e3d10c31428e503

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/pickleball_bracket_maker/sw.js', { scope: '/pickleball_bracket_maker/' })})}
