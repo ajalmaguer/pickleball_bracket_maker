fa08698185bad47f239e83147068a4ef0c026dfc
ajalmaguer <ajalmaguer@users.noreply.github.com>
2026-08-12 20:49:04 +0000

    deploy: e96ee77bcbe80b0ffa9374d94ee99fe583917956

---
+++ assets/4-players-CGs2Wu-x.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-CLwWq5Ua.js";function e(){return r.jsx("div",{children:"4 players page works"})}export{e as component};
---
+++ assets/9-12-players-BXloUhOk.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CLwWq5Ua.js";function o(){return e.jsx("div",{children:"9 - 12 players page works"})}export{o as component};
