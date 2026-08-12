68bbb2548796b41b058ed0bda981c92333582a5d
AJ Almaguer <ajalmaguer@gmail.com>
2026-08-12 15:09:43 -0700

    reordered mixed doubles

--- src/routes/5-8-players-mixed.tsx
+++ src/routes/5-8-players-mixed.tsx
@@ -32,10 +32,10 @@ const mixedDoubles: RoundRobin = [
     { serve: [4, 8], receive: [2, 6] },
     { serve: [3, 7], receive: [1, 5] },
   ],
-  [
-    { serve: [3, 7], receive: [4, 8] },
-    { serve: [2, 6], receive: [1, 5] },
-  ],
+  // [
+  //   { serve: [3, 7], receive: [4, 8] },
+  //   { serve: [2, 6], receive: [1, 5] },
+  // ],
 
   // 1 > 1, 6
   // 2 > 2, 7
@@ -49,10 +49,10 @@ const mixedDoubles: RoundRobin = [
     { serve: [4, 5], receive: [2, 7] },
     { serve: [3, 8], receive: [1, 6] },
   ],
-  [
-    { serve: [3, 8], receive: [4, 5] },
-    { serve: [2, 7], receive: [1, 6] },
-  ],
+  // [
+  //   { serve: [3, 8], receive: [4, 5] },
+  //   { serve: [2, 7], receive: [1, 6] },
+  // ],
 
   // 1 > 1, 7
   // 2 > 2, 8
@@ -66,10 +66,10 @@ const mixedDoubles: RoundRobin = [
     { serve: [4, 6], receive: [2, 8] },
     { serve: [3, 5], receive: [1, 7] },
   ],
-  [
-    { serve: [3, 5], receive: [4, 6] },
-    { serve: [2, 8], receive: [1, 7] },
-  ],
+  // [
+  //   { serve: [3, 5], receive: [4, 6] },
+  //   { serve: [2, 8], receive: [1, 7] },
+  // ],
 
   // 1 > 1, 8
   // 2 > 2, 5
@@ -82,6 +82,24 @@ const mixedDoubles: RoundRobin = [
   [
     { serve: [4, 7], receive: [2, 5] },
     { serve: [3, 6], receive: [1, 8] },
+  ],
+  // [
+  //   { serve: [3, 6], receive: [4, 7] },
+  //   { serve: [2, 5], receive: [1, 8] },
+  // ],
+
+  // extra if we have time - third round from each one
+  [
+    { serve: [3, 7], receive: [4, 8] },
+    { serve: [2, 6], receive: [1, 5] },
+  ],
+  [
+    { serve: [3, 8], receive: [4, 5] },
+    { serve: [2, 7], receive: [1, 6] },
+  ],
+  [
+    { serve: [3, 5], receive: [4, 6] },
+    { serve: [2, 8], receive: [1, 7] },
   ],
   [
     { serve: [3, 6], receive: [4, 7] },
