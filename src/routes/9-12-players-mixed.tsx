8e332c02764572d178215063c3241c862791bd9a
AJ Almaguer <ajalmaguer@gmail.com>
2026-08-12 17:29:07 -0700

    fixed 9-12 players mixed

--- src/routes/9-12-players-mixed.tsx
+++ src/routes/9-12-players-mixed.tsx
@@ -26,44 +26,44 @@ const courtStorage = atomWithStorage('9-12-players-mixed--courts', [
 
 const roundRobin: RoundRobin = [
   [
-    { serve: [6, 8], receive: [4, 9] },
-    { serve: [3, 8], receive: [4, 12] },
-    { serve: [2, 9], receive: [3, 10] },
+    { serve: [2, 7], receive: [1, 8] },
+    { serve: [3, 9], receive: [5, 11] },
+    { serve: [6, 12], receive: [4, 10] },
   ],
   [
-    { serve: [1, 8], receive: [2, 12] },
-    { serve: [2, 12], receive: [5, 10] },
-    { serve: [1, 11], receive: [6, 8] },
+    { serve: [6, 9], receive: [2, 10] },
+    { serve: [4, 8], receive: [1, 12] },
+    { serve: [3, 7], receive: [5, 11] },
   ],
   [
-    { serve: [4, 9], receive: [6, 12] },
-    { serve: [6, 7], receive: [1, 8] },
-    { serve: [2, 10], receive: [3, 7] },
+    { serve: [3, 10], receive: [2, 12] },
+    { serve: [4, 11], receive: [6, 7] },
+    { serve: [1, 8], receive: [5, 9] },
   ],
   [
-    { serve: [4, 8], receive: [1, 7] },
-    { serve: [2, 10], receive: [1, 9] },
-    { serve: [5, 10], receive: [3, 12] },
+    { serve: [4, 8], receive: [5, 10] },
+    { serve: [2, 9], receive: [3, 11] },
+    { serve: [6, 12], receive: [1, 7] },
   ],
   [
-    { serve: [3, 7], receive: [1, 9] },
-    { serve: [2, 9], receive: [4, 11] },
-    { serve: [5, 12], receive: [4, 11] },
-  ],
-  [
+    { serve: [2, 8], receive: [6, 9] },
+    { serve: [1, 11], receive: [5, 12] },
     { serve: [3, 10], receive: [4, 7] },
-    { serve: [6, 9], receive: [2, 7] },
-    { serve: [3, 9], receive: [1, 11] },
   ],
   [
-    { serve: [5, 12], receive: [2, 7] },
-    { serve: [5, 11], receive: [4, 10] },
-    { serve: [2, 8], receive: [6, 11] },
+    { serve: [4, 10], receive: [1, 9] },
+    { serve: [2, 7], receive: [5, 12] },
+    { serve: [3, 11], receive: [6, 8] },
   ],
   [
-    { serve: [5, 11], receive: [1, 10] },
-    { serve: [5, 7], receive: [6, 11] },
-    { serve: [4, 12], receive: [2, 11] },
+    { serve: [5, 9], receive: [4, 12] },
+    { serve: [2, 10], receive: [1, 11] },
+    { serve: [3, 8], receive: [6, 7] },
+  ],
+  [
+    { serve: [5, 10], receive: [2, 11] },
+    { serve: [1, 12], receive: [6, 8] },
+    { serve: [3, 7], receive: [4, 9] },
   ],
 ];
 
